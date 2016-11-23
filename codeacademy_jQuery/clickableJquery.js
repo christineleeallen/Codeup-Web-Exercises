ccb68b47ed5c17d75de5092c8664b005946e8e28
Christine Allen <christyallen000@gmail.com>
2016-11-22 21:51:50 -0600

    if else exercise, errors

--- codeacademy_jQuery/clickableJquery.js
+++ codeacademy_jQuery/clickableJquery.js
@@ -5,15 +5,21 @@
 function main() {
     $('.skillset').hide();
     $('.skillset').fadeIn(1000);
+    //makes whole page fade in. Can change milliseconds.  Default is (400)
 
     $('.projects').hide();
-
     $('.projects-button').on('click', function() {
-        $('.projects').toggle();
+        $(this).next().toggle();
+        //.next will move it to next toggle button
+
         $(this).toggleClass('active');
         //Class('active') added to toggle makes the button color change when active. 'active' came from the CSS stylesheet: .active
         //$(this) selects clicked element
+
+        $(this).text('TESTING');
+        //change text of DOM element on button when it's clicked
     });
 }
 
 $(document).ready(main);
+//jQuery connection
---
+++ codeacademy_jQuery/clickableSlideToggleJquery.js
@@ -0,0 +1,20 @@
+/**
+ * Created by home on 11/22/16.
+ */
+
+
+//function main() {
+//$('.skillset').hide();
+//$('.skillset').fadeIn(1000);
+
+//$('.projects').hide();
+
+//$('.projects-button').on('click', function() {
+    //$(this).next().slideToggle(400);
+    //slide toggle the projects in & out
+    //$(this).toggleClass('active');
+    //$(this).text('Projects Viewed');
+//});
+//
+
+//$(document).ready(main);
